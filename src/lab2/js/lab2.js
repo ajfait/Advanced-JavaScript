ade7b96541d5337117d9ff83f039d7ae96e68b0a
ajfait <jafait1004@gmail.com>
2025-01-27 20:22:09 -0600

    add comments

--- src/lab2/js/lab2.js
+++ src/lab2/js/lab2.js
@@ -1,5 +1,7 @@
+// Create global variable
 let labTitle = "Programming for Designers - Lab 2";
 
+// Log to the console
 const init = () => {
   let id = 10;
   let firstName = "Alison";
@@ -10,4 +12,5 @@ const init = () => {
   printLabTitle();
 };
 
+// Log to the console
 const printLabTitle = () => console.log(labTitle);
