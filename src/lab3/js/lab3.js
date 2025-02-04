9335a4eb4519b48c328f0bcff56da54f9303c2b3
ajfait <jafait1004@gmail.com>
2025-02-03 19:28:02 -0600

    create elements

--- src/lab3/js/lab3.js
+++ src/lab3/js/lab3.js
@@ -1,1 +1,6 @@
-const init = () => {};
+const init = () => {
+  let form = document.createElement("form");
+  let div = document.createElement("div");
+  let input = document.createElement("input");
+  let button = document.createElement("button");
+};
