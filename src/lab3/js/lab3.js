9f0067eed574f61c3cb30d5ae88e3ad4c7f19798
ajfait <jafait1004@gmail.com>
2025-02-03 19:37:07 -0600

    set attributes

--- src/lab3/js/lab3.js
+++ src/lab3/js/lab3.js
@@ -1,7 +1,15 @@
 const init = () => {
   let form = document.createElement("form");
+
   let div = document.createElement("div");
+  div.setAttribute("class", "form-group");
+
   let input = document.createElement("input");
+  input.setAttribute("class", "form-control");
+  input.setAttribute("type", "text");
+  input.setAttribute("id", "username");
+
   let button = document.createElement("button");
+  button.setAttribute("type", "button");
   button.innerHTML = "Click Me";
 };
