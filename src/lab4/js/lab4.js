99243419e4223ab7dd38181f409011b76ea34227
ajfait <jafait1004@gmail.com>
2025-02-09 15:50:26 -0600

    add documentation and compress lab 4 folder

--- src/lab4/js/lab4.js
+++ src/lab4/js/lab4.js
@@ -1,3 +1,6 @@
+/**
+ * Calls the dotNotation and addEventListener functions when the buttons are clicked
+ */
 const init = () => {
   document
     .querySelectorAll(".dotNotation")
@@ -10,20 +13,38 @@ const init = () => {
   document
     .querySelectorAll(".addEventListener")
     .forEach(function (buttonAddEventListener) {
-      buttonAddEventListener.addEventListener("click", addEventListener);
+      buttonAddEventListener.addEventListener("click", w3c);
     });
 };
 
+/**
+ * Logs inline button values to the console using onClick
+ *
+ * @param {*} control
+ */
 const inline = (control) => {
   console.log(control.innerHTML);
 };
 
+/**
+ * Logs dotNotation button values to the console
+ *
+ * @param {*} button
+ */
 const dotNotation = (button) => {
   console.log(button.innerHTML);
 };
 
-const addEventListener = (event) => {
+/**
+ * Logs addEventListener button values to the console
+ *
+ * @param {*} event
+ */
+const w3c = (event) => {
   console.log(event.currentTarget.innerHTML);
 };
 
+/**
+ * Calls the init() method on page load
+ */
 window.onload = init;
