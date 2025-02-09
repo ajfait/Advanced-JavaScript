45c8e98f5634f816e113a4c32efb1170016fcbdc
ajfait <jafait1004@gmail.com>
2025-02-09 12:50:22 -0600

    create dotNoation and addEventListener functions

--- src/lab4/js/lab4.js
+++ src/lab4/js/lab4.js
@@ -1,7 +1,29 @@
-const init = () => {};
+const init = () => {
+  document
+    .querySelectorAll(".dotNotation")
+    .forEach(function (buttonDotNotation) {
+      buttonDotNotation.onclick = function () {
+        dotNotation(buttonDotNotation);
+      };
+    });
+
+  document
+    .querySelectorAll(".addEventListener")
+    .forEach(function (buttonAddEventListener) {
+      buttonAddEventListener.addEventListener("click", addEventListener);
+    });
+};
 
 const inline = (control) => {
   console.log(control.innerHTML);
+};
+
+const dotNotation = (button) => {
+  console.log(button.innerHTML);
+};
+
+const addEventListener = (event) => {
+  console.log(event.currentTarget.innerHTML);
 };
 
 window.onload = init;
