e306bcc2b8bc419a9ecedf068588a27d766ac736
ajfait <jafait1004@gmail.com>
2025-02-25 19:34:18 -0600

    convert temperature

--- src/project2/js/project2.js
+++ src/project2/js/project2.js
@@ -1,46 +1,69 @@
+"use strict";
+
+const username = "ajfait";
+
 const init = () => {
-    let weatherButton = document.querySelector("#getWeather");
-    weatherButton.addEventListener("click", getLocation);
-}
+  let weatherButton = document.querySelector("#getWeather");
+  weatherButton.addEventListener("click", getLocation);
+};
 
 const getWeather = (lat, lng) => {
-    let xhr = new XMLHttpRequest();
-    let url = `https://secure.geonames.org/findNearByWeatherJSON?username=ajfait&lat=${lat}&lng=${lng}`;
+  let xhr = new XMLHttpRequest();
+  let url = `https://secure.geonames.org/findNearByWeatherJSON?username=${username}&lat=${lat}&lng=${lng}`;
 
-    xhr.open("get", url);
+  xhr.open("get", url);
 
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState == 4) {
-            console.log(url);
-        }
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      console.log(url);
+
+      let data = JSON.parse(xhr.responseText);
+      let temperature = data.weatherObservation.temperature;
+
+      console.log(`Temperature (Celsius): ${temperature}`);
+
+      convertTemperature(temperature);
     }
+  };
 
-    xhr.send(null);
-}
+  xhr.send(null);
+};
 
 const getLocation = () => {
-    let xhr = new XMLHttpRequest();
-    let zip = document.querySelector("#zip").value;
-    let url = `https://secure.geonames.org/postalCodeSearchJSON?username=ajfait&postalcode=${zip}`;
+  let xhr = new XMLHttpRequest();
+  let zip = document.querySelector("#zip").value;
+  let url = `https://secure.geonames.org/postalCodeSearchJSON?username=${username}&postalcode=${zip}`;
 
-    console.log(url);
+  console.log(url);
 
-    xhr.open("get", url);
+  xhr.open("get", url);
 
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState == 4) {
-            let data = JSON.parse(xhr.responseText);
-            let lat = data.postalCodes[0].lat;
-            let lng = data.postalCodes[0].lng;
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      let data = JSON.parse(xhr.responseText);
+      let lat = data.postalCodes[0].lat;
+      let lng = data.postalCodes[0].lng;
 
-            console.log(lat);
-            console.log(lng);
-            
-            getWeather(lat, lng);
-        }
+      console.log(`Latitude: ${lat}`);
+      console.log(`Longitude: ${lng}`);
+
+      getWeather(lat, lng);
     }
+  };
 
-    xhr.send(null);
-}
+  xhr.send(null);
+};
+
+const convertTemperature = (temperature) => {
+  //°F = (°C × 9/5) + 32
+  const CONVERSION_FACTOR = 1.8;
+  const OFFSET = 32;
+
+  let temperatureFahrenheit = temperature * CONVERSION_FACTOR + OFFSET;
+
+  console.log(`Temperature (Fahrenheit): ${temperatureFahrenheit}`);
+
+  return temperatureFahrenheit;
+};
 
 window.onload = init;
