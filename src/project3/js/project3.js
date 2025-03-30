690d3f5069d6d684a6560944f02640fc8ec6fe25
ajfait <jafait1004@gmail.com>
2025-03-30 11:36:03 -0500

    test add task button

--- src/project3/js/project3.js
+++ src/project3/js/project3.js
@@ -1,5 +1,16 @@
+// Create global variables
+const url = "https://ghu8xhzgfe.execute-api.us-east-1.amazonaws.com/tasks/";
+const studentId = 2886897;
+const apiKey = "Itcheui2tB58SlUGe8rrP8mskudGsNDT9nfKKG9S";
+const contentType = "application/json";
+
 const init = () => {
-  console.log("project 3");
+  document.querySelector("#newTask").addEventListener("click", addNewTask);
+};
+
+const addNewTask = () => {
+  const taskDescription = document.querySelector("#task").value;
+  console.log(taskDescription);
 };
 
 window.onload = init;
