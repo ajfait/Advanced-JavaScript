ae575085b857b50a28b44daceaf12e362ed267f1
ajfait <jafait1004@gmail.com>
2025-03-30 12:28:12 -0500

    create and test addTask method

--- src/project3/js/project3.js
+++ src/project3/js/project3.js
@@ -1,16 +1,31 @@
 // Create global variables
 const url = "https://ghu8xhzgfe.execute-api.us-east-1.amazonaws.com/tasks/";
-const studentId = 2886897;
-const apiKey = "Itcheui2tB58SlUGe8rrP8mskudGsNDT9nfKKG9S";
-const contentType = "application/json";
+const student_id = "2886897";
+const api_key = "Itcheui2tB58SlUGe8rrP8mskudGsNDT9nfKKG9S";
+const content_type = "application/json";
 
 const init = () => {
-  document.querySelector("#newTask").addEventListener("click", addNewTask);
+  document.querySelector("#newTask").addEventListener("click", addTask);
+  displayTasks();
 };
 
-const addNewTask = () => {
+const addTask = async () => {
   const taskDescription = document.querySelector("#task").value;
-  console.log(taskDescription);
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": content_type, "x-api-key": api_key },
+      body: JSON.stringify({
+        StudentId: student_id,
+        Description: taskDescription,
+      }),
+    });
+  } catch (error) {}
 };
+
+const displayTasks = () => {};
+
+const deleteTask = () => {};
 
 window.onload = init;
